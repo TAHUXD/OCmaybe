c4b642d7dc44013d5c51c6fb486b1e34522fb451
Abdul Rehman <arehmannasir@outlook.com>
2022-04-04 10:26:54 +0100

    publish positions of static benches

--- src/robot_detection/gps_simulator.py
+++ src/robot_detection/gps_simulator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import queue
+from turtle import position
 from typing import Dict
 import roslib
 import sys
@@ -40,6 +41,8 @@ class GPS_SIMULATOR:
     # initialize a publisher to send robot coordinates in the system's frame.
     self.pos_pub = rospy.Publisher("/robot_position", Point ,queue_size = 1)
     # initialize a publisher to send robot coordinates in the world frame.
+    self.bench2_pub = rospy.Publisher('/bench2_position_longlat', Point, queue_size=1)
+    self.bench3_pub = rospy.Publisher('/bench3_position_longlat', Point, queue_size=1)
     self.pos_pub_longlat = rospy.Publisher('/robot_position_longlat', Point, queue_size=1)
 
     self.cap = cv2.VideoCapture(0)
@@ -54,27 +57,44 @@ class GPS_SIMULATOR:
         frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
         cv2.imwrite('camera_input.jpg', frame)
 
-        robot_position, angle = self._processor.runProcessor(frame)
-        position_in_point = Point(float(robot_position[1]), float(robot_position[0]), angle)
+        robot_position, angle, position_yellow, position_green = self._processor.runProcessor(frame)
+        position_in_point_robot = Point(float(robot_position[1]), float(robot_position[0]), angle)
+        position_in_point_bench2 = Point(float(position_yellow[1]), float(position_yellow[0]), angle)
+        position_in_point_bench3 = Point(float(position_green[1]), float(position_green[0]), angle)
         # print(position_in_point)
 
-        change_in_Long = self.LONG_MAX - self.LONG_MIN
-        change_in_lat = self.LAT_MAX - self.LAT_MIN
-        long_conversion_constant = change_in_Long / self.IMAGE_Y
-        lat_conversion_constant = change_in_lat / self.IMAGE_X
+        # change_in_Long = self.LONG_MAX - self.LONG_MIN
+        # change_in_lat = self.LAT_MAX - self.LAT_MIN
+        # long_conversion_constant = change_in_Long / self.IMAGE_Y
+        # lat_conversion_constant = change_in_lat / self.IMAGE_X
 
-        point_to_convert = copy.deepcopy(position_in_point)
+        # point_to_convert = copy.deepcopy(position_in_point)
         
-        point_to_convert.long = long_conversion_constant * point_to_convert.long
-        point_to_convert.lat = lat_conversion_constant * point_to_convert.lat
+        # point_to_convert.long = long_conversion_constant * point_to_convert.long
+        # point_to_convert.lat = lat_conversion_constant * point_to_convert.lat
 
         #print(position_in_point)
-        self.pos_pub.publish(position_in_point)
-        self.pos_pub_longlat.publish(point_to_convert)
-        print(point_to_convert)
+        self.pos_pub.publish(position_in_point_robot)
+        self.pos_pub_longlat.publish(self.convert_to_longlat(position_in_point_robot))
+        self.bench2_pub.publish(self.convert_to_longlat(position_in_point_bench2))
+        self.bench3_pub.publish(self.convert_to_longlat(position_in_point_bench3))
+        print(self.convert_to_longlat(position_in_point_robot))
 
     cap.release()
     cv2.destroyAllWindows()
+  
+  def convert_to_longlat(self, position_in_point):
+    change_in_Long = self.LONG_MAX - self.LONG_MIN
+    change_in_lat = self.LAT_MAX - self.LAT_MIN
+    long_conversion_constant = change_in_Long / self.IMAGE_Y
+    lat_conversion_constant = change_in_lat / self.IMAGE_X
+
+    point_to_convert = copy.deepcopy(position_in_point)
+    
+    point_to_convert.long = long_conversion_constant * point_to_convert.long
+    point_to_convert.lat = lat_conversion_constant * point_to_convert.lat
+
+    return point_to_convert
 
         
 
